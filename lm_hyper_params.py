05cebfab3e4248baca637bc95cffcc7e2038daa4
Chauncey Wang <chncwang@gmail.com>
2020-08-05 19:18:53 +0800

    add new files

---
+++ lm_hyper_params.py
@@ -0,0 +1,11 @@
+seed = 0
+batch_size = 128
+embedding_tuning = False
+min_freq = 100
+word_dim = 300
+hidden_dim = 1024
+dropout = 0.1
+learning_rate = 3e-4
+weight_decay = 1e-5
+clip_grad = 1e1
+layer = 1
