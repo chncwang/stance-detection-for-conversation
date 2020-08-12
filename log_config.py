75832ec89c9617ed7d383b5b1e943756cd6f6b78
Chauncey Wang <chncwang@gmail.com>
2020-08-12 19:07:03 +0800

    modify git ignore to commit log_config

---
+++ log_config.py
@@ -0,0 +1,16 @@
+from logging import getLogger, DEBUG, INFO, CRITICAL, ERROR, WARNING
+import logging
+import datetime
+
+FORMAT = "%(levelname)s - %(asctime)s - %(filename)s - %(funcName)s -\
+%(lineno)d - %(message)s"
+LOG_FILENAME = "log-{}".format(datetime.datetime.now()).replace(" ", "-")
+logging.basicConfig(format = FORMAT,
+#         filename = LOG_FILENAME,
+        level = logging.INFO)
+
+# getLogger("train").setLevel(DEBUG)
+# getLogger("model").setLevel(DEBUG)
+# getLogger("dataset").setLevel(DEBUG)
+# getLogger("lm_train").setLevel(DEBUG)
+# getLogger("lm_module").setLevel(DEBUG)
